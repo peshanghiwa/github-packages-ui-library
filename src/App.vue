dfc7995c4b04e2fd42932903317b108722d217a6
Peshang Hiwa <peshanghiwa0@gmail.com>
2023-01-29 20:24:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { BaseButton } from "./index";
+</script>
+
+<template>
+  <BaseButton> Click me </BaseButton>
+</template>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from "vue";
+type BaseButtonProps = {
+  size?: number;
+  color?: string;
+};
+
+const props = withDefaults(defineProps<BaseButtonProps>(), {
+  size: 16,
+  color: "skyblue",
+});
+
+const fontSize = computed(() => `${props.size}px`);
+</script>
+
+<template>
+  <button class="base-button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  background-color: v-bind(color);
+  font-size: v-bind(fontSize);
+  padding: 1rem 2rem;
+  cursor: pointer;
+  border: none;
+}
+</style>
